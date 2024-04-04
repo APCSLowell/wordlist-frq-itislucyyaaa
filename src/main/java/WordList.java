8209673683b3e5f070133e620336f28e688ebd05
itislucyyaaa <142839187+itislucyyaaa@users.noreply.github.com>
2024-04-04 11:48:09 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -11,12 +11,23 @@ public class WordList
   public int numWordsOfLength(int len)
   {
     /* to be implemented in part (a) */
+    for(int i=0; i<myList.size(); i++){
+      if(myList.get(myList.get((i).size()))==len){
+        num++;
+      }
+    }
     
   }
 
   public void removeWordsOfLength(int len)
   {
     /* to be implemented in part (b) */
-
-  }
+    ArrayList <String> Aa = new ArrayList <String>();
+    int n=0;
+    for(int i=0; i<myList.size(); i++){
+      if(myList.get(myList.get((i).size()))!=len){
+        Aa[n]=myList.get(i);
+        n++;
+      }
+    }
 }
